e2cf027c158380ec3c31cd003cb7a41d2fd59cd6
Mundrack <mateopuga75@gmail.com>
2024-12-16 10:15:03 -0500

    Agregamos para ver la bateria y ahora estamos por crear para usar la camara

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,6 +20,21 @@
             Vibration.Vibrate();
 
         }
+
+        public void GetBateriInfo(object sender, EventArgs e)
+        {
+            var level = Battery.ChargeLevel;
+            var State = Battery.State;
+            var source = Battery.PowerSource;
+
+            InfoBatery.Text = $"Nivel de bateria: {level}\n Estado: {State}\n Source: {source}";
+
+        }
+
+        private void GetPhoto(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
